3b5146e2c4c9bcf739490eba389c3a9f4a574696
Jack Franklin <jack@jackfranklin.net>
2012-03-06 23:34:59 +0000

    Edited it so it takes a file as an input & modifies the file directly

--- markdowntwitter.rb
+++ markdowntwitter.rb
@@ -1,15 +1,20 @@
 #! /usr/bin/ruby
 #
-# Usage: $ cat yourpost.md | ./markdowntwitter.rb
+# Usage: ./markdowntwitter.rb yourtext.md
+# yourtext.md will be modified accordingly
+# any instances of @username are replaced with [@username](http://twitter.com/username)
 #
 # Written by Jack Franklin
 
-input_file = ARGF.read
-puts "Inputted Text \n #{input_file}"
-
-input_file.gsub!(/\s@(\w)+\s/) do |match|
-  match.strip!
+input_file = ARGV[0]
+contents = File.read(input_file)
+contents.gsub!(/\s@(\w)+\s/) do |match|
+  match.strip! #whitespace, be gone!
   " [#{match}](http://twitter.com/#{match[1..-1]}/) "
 end
 
-puts "Outputted Text \n #{input_file}"
+File.open(input_file, "w") { |file|
+  file.puts contents
+}
+
+puts "File #{input_file} was modified. Output: \n #{contents}"
--- test.md
+++ test.md
@@ -1,1 +1,2 @@
-So I was talking to @46bit one day
+So I was talking to [@46bit](http://twitter.com/46bit/) one day and I asked him to email
+jack@jackfranklin.net. Totally awesome and then [@richquick](http://twitter.com/richquick/) turned up.
